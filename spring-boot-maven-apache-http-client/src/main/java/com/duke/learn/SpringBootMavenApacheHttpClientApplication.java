35758d84a70f027048dfe4eb45cfbbd00cd7a72a
Duke <duke.yu@derbysoft.com>
2019-03-05 13:05:31 +0800

    init apache http client

---
+++ spring-boot-maven-apache-http-client/src/main/java/com/duke/learn/SpringBootMavenApacheHttpClientApplication.java
@@ -0,0 +1,14 @@
+package com.duke.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenApacheHttpClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMavenApacheHttpClientApplication.class, args);
+		System.out.println("init project");
+	}
+
+}
